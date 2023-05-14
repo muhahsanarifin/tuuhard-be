baf9ed1ff447240939dd5a0e79a9018a66f1b9dc
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-05-15 03:53:58 +0700

    Add main.js

---
+++ src/routes/main.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const authRouter = require("./auth");
+
+const mainRouter = express.Router();
+
+mainRouter.use("/auth", authRouter);
+
+module.exports = mainRouter;
