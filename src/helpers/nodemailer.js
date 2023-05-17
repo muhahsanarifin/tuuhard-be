4bc739a3fbcdd1047a55d307f2f45c23c2582c3c
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-05-17 11:21:12 +0700

    Update helper

--- src/helpers/nodemailer.js
+++ src/helpers/nodemailer.js
@@ -26,7 +26,7 @@ const nodemailer = require("nodemailer");
 //   <body style="font-family: Arial, sans-serif; font-size: 16px;">
 //     <h2 style="text-align: center; color: #A5D7E8;">Thank You for Registering!</h2>
 //     <p style="font-weight: 700;">Dear ${email},</p>
-//     <p>We are pleased to confirm your registration. Please click the <a href="${process.env.URL_DIRECT_TO_CONFIRM}/?token=${token}" style="color: #A5D7E8; font-weight: 700;">link</a></p>
+//     <p>We are pleased to confirm your registration. Please click the <a href="${process.env.URL_DIRECT}/.../?token=${token}" style="color: #A5D7E8; font-weight: 700;">link</a></p>
 //     <p>If you have any questions or concerns, please do not hesitate to contact us at <span style="font-weight:600;">yukduit@gmail.com</span>.</p>
 //     <p>Thank you again for registering.</p>
 //     <p>Best regards,<br><span style="font-weight: 700;">tuuhard team</span></p>
@@ -63,13 +63,48 @@ module.exports = {
       <body style="font-family: Arial, sans-serif; font-size: 16px;">
         <h2 style="text-align: center; color: #A5D7E8;">Thank You for Registering!</h2>
         <p style="font-weight: 700;">Dear ${response.email},</p>
-        <p>We are pleased to confirm your registration. Please click the <a href="${process.env.URL_DIRECT_TO_CONFIRM}/?token=${token}" style="color: #A5D7E8; font-weight: 700;">link</a></p>
+        <p>We are pleased to confirm your registration. Please click the <a href="${process.env.URL_DIRECT}/auth/confirm/?token=${token}" style="color: #A5D7E8; font-weight: 700;">link</a></p>
         <p>If you have any questions or concerns, please do not hesitate to contact us at <span style="font-weight:600;">yukduit@gmail.com</span>.</p>
         <p>Thank you again for registering.</p>
         <p>Best regards,<br><span style="font-weight: 700;">tuuhard team</span></p>
       </body>
     </html>`,
     };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return console.log(err);
+      }
+      return console.log(info.response);
+    });
+  },
+  resetmailer: (email, token) => {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_SENDER,
+        pass: process.env.PASSWORD_SENDER,
+      },
+    });
+    const mailOptions = {
+      from: process.env.EMAIL_SENDER,
+      to: email,
+      subject: "Tuuhard | Reset Password",
+      html: `<html>
+      <head>
+        <meta charset="utf-8">
+        <title>Reset Password Request</title>
+      </head>
+      <body style="font-family: Arial, sans-serif; font-size: 16px;">
+        <h2 style="text-align: center; color: #A5D7E8;">Reset Password</h2>
+        <p style="font-weight: 700;">Dear ${email},</p>
+        <p>You are receiving this email because you requested a password reset for your account. Please click the <a href="${process.env.URL_DIRECT}/password/reset/${token}" style="color: #A5D7E8; font-weight: 700;">link</a> to reset your password. The link only valid 5 minutes.</p>
+        <p>If you have any questions or concerns, please do not hesitate to contact us at <span style="font-weight:600;">yukduit@gmail.com</span>.</p>
+        <p>Best regards,<br><span style="font-weight: 700;">tuuhard team</span></p>
+      </body>
+    </html>`,
+    };
+
     transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
         return console.log(err);
