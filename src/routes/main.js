a5708b35672875084e4a9b659b46020c4454d195
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-05-17 11:22:14 +0700

    Update main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const authRouter = require("./auth");
 
+const passwordRouter = require("./passwords");
+
 const mainRouter = express.Router();
 
 mainRouter.use("/auth", authRouter);
+mainRouter.use("/password", passwordRouter);
 
 module.exports = mainRouter;
