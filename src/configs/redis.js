2adf9fc6c9f9749dffc0bd2a25c49d97ffb54b5d
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-05-23 10:56:20 +0700

    Update redis.js

--- src/configs/redis.js
+++ src/configs/redis.js
@@ -1,27 +1,5 @@
 const redis = require("redis");
 
-// let client;
-
-// client = null;
-
-// if (!client) {
-//   client = redis.createClient({
-//     url: process.env.REDIS_URL,
-//   });
-
-//   client.on("connect", () => {
-//     console.log("Connected to Redis.");
-//   });
-
-//   client.on("error", (err) => {
-//     console.log("Error:", err);
-//   });
-// }
-
-// client.connect().then();
-
-// module.exports = client;
-
 const client = redis.createClient({
   url: process.env.REDIS_URL,
 });
