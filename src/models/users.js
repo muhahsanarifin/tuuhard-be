8d996a5fd0c4861c62959eca7f90aae63a46e796
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-05-25 22:42:07 +0700

    Update users.js

--- src/models/users.js
+++ src/models/users.js
@@ -5,7 +5,7 @@ module.exports = {
     const { email } = body;
     return new Promise((resolve, reject) => {
       const query =
-        "select u.id, u.email, u.password, r.role, u.last_login, u.created_at, u.updated_at from users u join roles r on u.role_id = r.id where u.email = $1";
+        "SELECT u.id, u.email, u.password, r.role, u.last_login, u.created_at, u.updated_at FROM users u JOIN roles r ON u.role_id = r.id WHERE u.email = $1";
 
       db.query(query, [email], (error, result) => {
         if (error) {
@@ -19,13 +19,38 @@ module.exports = {
     const { email } = body;
     return new Promise((resolve, reject) => {
       const query =
-        "select u.id,u.email, r.role, status_account from users u join accounts a on u.id = a.user_id join roles r on u.role_id = r.id where u.email = $1";
+        "SELECT u.id,u.email, r.role, status_account FROM users u JOIN accounts a ON u.id = a.user_id JOIN roles r ON u.role_id = r.id WHERE u.email = $1";
 
       db.query(query, [email], (error, result) => {
         if (error) {
           return reject(error);
         }
         return resolve(result);
+      });
+    });
+  },
+  retriveProfiles: () => {
+    return new Promise((resolve, reject) => {
+      const query =
+        "SELECT p.user_id, p.first_name, p.last_name, p.gender, p.birth, p.no_telp, p.created_at, p.updated_at FROM profiles p";
+      db.query(query, (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(result);
+      });
+    });
+  },
+  retriveProfile: (payload) => {
+    const { id } = payload;
+    return new Promise((resolve, reject) => {
+      const query =
+        "SELECT p.user_id, p.first_name, p.last_name, p.gender, p.birth, p.no_telp, p.created_at, p.updated_at FROM profiles p WHERE p.user_id = $1";
+      db.query(query, [id], (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(result);
       });
     });
   },
